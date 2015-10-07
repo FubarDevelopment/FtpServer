a10ff8ed694876e4a09dad2961831e5862b2c49a
Mark Junker <m.junker@fubar-dev.de>
2015-10-07 18:06:43 +0200

    Support for both data connection and direct output.

--- FubarDev.FtpServer/CommandHandlers/BlstCommandHandler.cs
+++ FubarDev.FtpServer/CommandHandlers/BlstCommandHandler.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="BlstCommandHandler.cs" company="Fubar Development Junker">
+//     Copyright (c) Fubar Development Junker. All rights reserved.
+// </copyright>
+// <author>Mark Junker</author>
+//-----------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
@@ -32,6 +39,38 @@ namespace FubarDev.FtpServer.CommandHandlers
         /// <inheritdoc/>
         public override async Task<FtpResponse> Process(FtpCommand command, CancellationToken cancellationToken)
         {
+            string mode = (string.IsNullOrEmpty(command.Argument) ? "data" : command.Argument).ToLowerInvariant();
+
+            switch (mode)
+            {
+                case "data":
+                    return await SendWithDataConnection(cancellationToken);
+                case "direct":
+                    return await SendDirectly(cancellationToken);
+            }
+
+            return new FtpResponse(501, $"Mode {mode} not supported.");
+        }
+
+        private async Task<FtpResponse> SendDirectly(CancellationToken cancellationToken)
+        {
+            var taskStates = Server.GetBackgroundTaskStates();
+            if (taskStates.Count == 0)
+                return await Task.FromResult(new FtpResponse(211, "No background tasks"));
+
+            await Connection.Write("211-Active background tasks:", cancellationToken);
+            foreach (var entry in taskStates)
+            {
+                var line = $"{entry.Item2.ToString().PadRight(12)} {entry.Item1}";
+                Connection.Log?.Debug(line);
+                await Connection.Write($" {line}", cancellationToken);
+            }
+
+            return await Task.FromResult(new FtpResponse(211, "END"));
+        }
+
+        private async Task<FtpResponse> SendWithDataConnection(CancellationToken cancellationToken)
+        {
             await Connection.Write(new FtpResponse(150, "Opening data connection."), cancellationToken);
             ITcpSocketClient responseSocket;
             try
@@ -42,6 +81,7 @@ namespace FubarDev.FtpServer.CommandHandlers
             {
                 return new FtpResponse(425, "Can't open data connection.");
             }
+
             try
             {
                 var encoding = Data.NlstEncoding ?? Connection.Encoding;
