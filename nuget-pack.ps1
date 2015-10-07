6e3b7fecf462192e65b8f2c815b4235eb7a91a39
Mark Junker <m.junker@fubar-dev.de>
2015-10-07 16:17:59 +0200

    Use explicit nuspec files (for optimal dependencies)

---
+++ nuget-pack.ps1
@@ -0,0 +1,12 @@
+[OutputType([void])]
+param(
+	[Parameter()]
+	$version = "1.0.0-beta02",
+	[Parameter()]
+	$config = "Release"
+)
+
+& nuget pack FubarDev.FtpServer\FubarDev.FtpServer.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
+& nuget pack FubarDev.FtpServer.AccountManagement\FubarDev.FtpServer.AccountManagement.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
+& nuget pack FubarDev.FtpServer.FileSystem\FubarDev.FtpServer.FileSystem.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
+& nuget pack FubarDev.FtpServer.FileSystem.GoogleDrive\FubarDev.FtpServer.FileSystem.GoogleDrive.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
