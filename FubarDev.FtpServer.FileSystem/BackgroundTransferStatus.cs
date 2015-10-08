31f73cde05573f44e17921bf5748cee8c4b8c84c
Mark Junker <m.junker@fubar-dev.de>
2015-10-08 11:04:32 +0200

    Updated license

--- FubarDev.FtpServer.FileSystem/BackgroundTransferStatus.cs
+++ FubarDev.FtpServer.FileSystem/BackgroundTransferStatus.cs
@@ -1,4 +1,11 @@
-﻿namespace FubarDev.FtpServer.FileSystem
+﻿//-----------------------------------------------------------------------
+// <copyright file="BackgroundTransferStatus.cs" company="Fubar Development Junker">
+//     Copyright (c) Fubar Development Junker. All rights reserved.
+// </copyright>
+// <author>Mark Junker</author>
+//-----------------------------------------------------------------------
+
+namespace FubarDev.FtpServer.FileSystem
 {
     /// <summary>
     /// The status of a single <see cref="IBackgroundTransfer"/>
