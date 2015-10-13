e11241a6f69e09ecc4a1501aae0d8900234da3a8
Mark Junker <m.junker@fubar-dev.de>
2015-10-13 15:22:03 +0200

    Bumped version

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -1,7 +1,7 @@
 [OutputType([void])]
 param(
 	[Parameter()]
-	$version = "1.0.0-beta15",
+	$version = "1.0.0-beta16",
 	[Parameter()]
 	$config = "Release"
 )
@@ -10,3 +10,4 @@ param(
 & nuget pack FubarDev.FtpServer.AccountManagement\FubarDev.FtpServer.AccountManagement.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
 & nuget pack FubarDev.FtpServer.FileSystem\FubarDev.FtpServer.FileSystem.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
 & nuget pack FubarDev.FtpServer.FileSystem.GoogleDrive\FubarDev.FtpServer.FileSystem.GoogleDrive.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
+& nuget pack FubarDev.FtpServer.FileSystem.DotNet\FubarDev.FtpServer.FileSystem.DotNet.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
