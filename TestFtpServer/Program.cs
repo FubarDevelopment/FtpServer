fcb26a480a0e4e6bee9922426f86a1a5e673864f
Mark Junker <m.junker@fubar-dev.de>
2015-10-13 14:57:39 +0200

    Simplification of the test FTP server

--- TestFtpServer/Program.cs
+++ TestFtpServer/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 using FubarDev.FtpServer;
 using FubarDev.FtpServer.AccountManagement;
@@ -15,43 +13,29 @@ namespace TestFtpServer
 {
     class Program
     {
-        private static readonly CancellationTokenSource _tokenSrc = new CancellationTokenSource();
-
         private static void Main()
         {
             var membershipProvider = new AnonymousMembershipProvider(new NoValidation());
             var fsProvider = new DotNetFileSystemProvider(Path.Combine(Path.GetTempPath(), "TestFtpServer"));
-            var server = new FtpServer(fsProvider, membershipProvider, "127.0.0.1")
+            using (var ftpServer = new FtpServer(fsProvider, membershipProvider, "127.0.0.1")
             {
                 DefaultEncoding = Encoding.ASCII,
                 LogManager = new FtpLogManager(),
-            };
-            Console.WriteLine("Press ENTER/RETURN to close the test application.");
-            Task.Run(() => Run(server));
-            Console.ReadLine();
-            _tokenSrc.Cancel();
-        }
-
-        private static void Run(FtpServer ftpServer)
-        {
-            var log = ftpServer.LogManager?.CreateLog(typeof(Program));
-
-            try
+            })
             {
-                ftpServer.Start();
-                do
+                Console.WriteLine("Press ENTER/RETURN to close the test application.");
+                var log = ftpServer.LogManager?.CreateLog(typeof(Program));
+
+                try
                 {
+                    ftpServer.Start();
+                    Console.ReadLine();
+                    ftpServer.Stop();
                 }
-                while (!_tokenSrc.Token.WaitHandle.WaitOne(200));
-                ftpServer.Stop();
-            }
-            catch (Exception ex)
-            {
-                log?.Error(ex, "Error during main FTP server loop");
-            }
-            finally
-            {
-                ftpServer.Dispose();
+                catch (Exception ex)
+                {
+                    log?.Error(ex, "Error during main FTP server loop");
+                }
             }
         }
     }
