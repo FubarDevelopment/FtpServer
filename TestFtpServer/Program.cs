04b360355c530e0fc55acce852669ec83c9a0aac
Mark Junker <m.junker@fubar-dev.de>
2018-05-24 16:37:40 +0200

    Changes to the sample application
    You can now use command line arguments to select the file system backend

--- TestFtpServer/Program.cs
+++ TestFtpServer/Program.cs
@@ -4,13 +4,15 @@
 
 using System;
 using System.IO;
+using System.Linq;
+using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml;
 
 using FubarDev.FtpServer;
-using FubarDev.FtpServer.AccountManagement.Anonymous;
 using FubarDev.FtpServer.FileSystem.DotNet;
 
 using Google.Apis.Auth.OAuth2;
@@ -18,6 +20,9 @@ using Google.Apis.Drive.v3;
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Mono.Options;
 
 using NLog.Extensions.Logging;
 
@@ -25,74 +30,112 @@ namespace TestFtpServer
 {
     internal static class Program
     {
-#if USE_FTPS_IMPLICIT
-        internal const int Port = 990;
-#else
-        internal const int Port = 21;
-#endif
-
-        private static async Task Main()
+        private static int Main(string[] args)
         {
-            // Load server certificate
-            var cert = new X509Certificate2("test.pfx");
+            var options = new TestFtpServerOptions();
 
-            var services = new ServiceCollection();
-            services.AddLogging(cfg => cfg.SetMinimumLevel(LogLevel.Trace));
-            services.AddOptions();
-
-            services.Configure<AuthTlsOptions>(opt => opt.ServerCertificate = cert);
-            services.Configure<FtpConnectionOptions>(opt => opt.DefaultEncoding = Encoding.ASCII);
-            services.Configure<FtpServerOptions>(opt =>
+            var optionSet = new CommandSet("ftpserver")
             {
-                opt.ServerAddress = "localhost";
-                opt.Port = Port;
-            });
-
-            services.AddSingleton<IAnonymousPasswordValidator, NoValidation>();
-
-            var provider = "filesystem";
-            if (provider == "filesystem")
-            {
-                services.Configure<DotNetFileSystemOptions>(opt => opt.RootPath = Path.Combine(Path.GetTempPath(), "TestFtpServer"));
-                services.AddFtpServer(sb => sb.UseDotNetFileSystem().EnableAnonymousAuthentication());
-            }
-            else if (provider == "google-service")
-            {
-                var credential = GoogleCredential
-                    .FromFile(@"service-credential.json")
-                    .CreateScoped(DriveService.Scope.Drive, DriveService.Scope.DriveFile);
-
-                services.AddFtpServer(
-                    sb =>
-                    {
-                        sb
-                            .UseGoogleDrive(credential)
-                            .EnableAnonymousAuthentication();
-                    });
-            }
-            else if (provider == "google-user")
-            {
-                string userEmail = string.Empty;
-                UserCredential credential;
-                using (var secretsSource = new FileStream("client_secrets.json",
-                    FileMode.Open))
+                "usage: ftpserver [OPTIONS] <COMMAND> [COMMAND-OPTIONS]",
+                { "h|?|help", "Show help", v => options.ShowHelp = v != null },
+                "Server",
+                { "a|address=", "Sets the IP address or host name", v => options.ServerAddress = v },
+                { "p|port=", "Sets the listen port", v => options.Port = Convert.ToInt32(v) },
+                "FTPS",
+                { "c|certificate=", "Set the SSL certificate", v => options.ServerCertificateFile =v },
+                { "P|password=", "Password for the SSL certificate", v => options.ServerCertificatePassword = v },
+                { "i|implicit", "Use implicit FTPS", v => options.ImplicitFtps = XmlConvert.ToBoolean(v.ToLowerInvariant()) },
+                "Backends",
+                new Command("filesystem", "Use the System.IO file system access")
                 {
-                    var secrets = GoogleClientSecrets.Load(secretsSource);
-                    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                        secrets.Secrets,
-                        new[] { DriveService.Scope.DriveFile, DriveService.Scope.Drive },
-                        userEmail, CancellationToken.None);
-                }
-
-                services.AddFtpServer(
-                    sb =>
+                    Options = new OptionSet()
                     {
-                        sb
-                            .UseGoogleDrive(credential)
-                            .EnableAnonymousAuthentication();
-                    });
+                        "usage: ftpserver filesystem [ROOT-DIRECTORY]",
+                    },
+                    Run = a => RunWithFileSystem(a.ToArray(), options),
+                },
+                new CommandSet("google-drive")
+                {
+                    new Command("user", "Use a users Google Drive as file system")
+                    {
+                        Options = new OptionSet()
+                        {
+                            "usage: ftpserver google-drive user <CLIENT-SECRETS-FILE> <USERNAME>",
+                        },
+                        Run = a => RunWithGoogleDriveUser(a.ToArray(), options).Wait(),
+                    },
+                    new Command("service", "Use a users Google Drive with a service account")
+                    {
+                        Options = new OptionSet()
+                        {
+                            "usage: ftpserver google-drive service <SERVICE-CREDENTIAL-FILE>",
+                        },
+                        Run = a => RunWithGoogleDriveService(a.ToArray(), options),
+                    },
+                },
+            };
+
+            return optionSet.Run(args);
+        }
+
+        private static void RunWithFileSystem(string[] args, TestFtpServerOptions options)
+        {
+            options.Validate();
+            var rootDir =
+                args.Length != 0 ? args[0] : Path.Combine(Path.GetTempPath(), "TestFtpServer");
+            var services = CreateServices(options)
+                .Configure<DotNetFileSystemOptions>(opt => opt.RootPath = rootDir)
+                .AddFtpServer(sb => Configure(sb).UseDotNetFileSystem());
+            Run(services, options);
+        }
+
+        private static async Task RunWithGoogleDriveUser(string[] args, TestFtpServerOptions options)
+        {
+            options.Validate();
+            if (args.Length != 2)
+            {
+                throw new Exception("This command requires two arguments: <CLIENT-SECRETS-FILE> <USERNAME>");
             }
 
+            var clientSecretsFile = args[0];
+            var userName = args[1];
+
+            UserCredential credential;
+            using (var secretsSource = new FileStream(clientSecretsFile, FileMode.Open))
+            {
+                var secrets = GoogleClientSecrets.Load(secretsSource);
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    secrets.Secrets,
+                    new[] { DriveService.Scope.DriveFile, DriveService.Scope.Drive },
+                    userName,
+                    CancellationToken.None);
+            }
+
+            var services = CreateServices(options)
+                .AddFtpServer(sb => Configure(sb).UseGoogleDrive(credential));
+            Run(services, options);
+        }
+
+        private static void RunWithGoogleDriveService(string[] args, TestFtpServerOptions options)
+        {
+            options.Validate();
+            if (args.Length != 1)
+            {
+                throw new Exception("This command requires one argument: <SERVICE-CREDENTIAL-FILE>");
+            }
+
+            var serviceCredentialFile = args[0];
+            var credential = GoogleCredential
+                .FromFile(serviceCredentialFile)
+                .CreateScoped(DriveService.Scope.Drive, DriveService.Scope.DriveFile);
+
+            var services = CreateServices(options)
+                .AddFtpServer(sb => Configure(sb).UseGoogleDrive(credential));
+            Run(services, options);
+        }
+
+        private static void Run(IServiceCollection services, TestFtpServerOptions options)
+        {
             using (var serviceProvider = services.BuildServiceProvider())
             {
                 var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
@@ -100,15 +143,19 @@ namespace TestFtpServer
                 NLog.LogManager.LoadConfiguration("NLog.config");
 
                 var ftpServer = serviceProvider.GetRequiredService<FtpServer>();
-#if USE_FTPS_IMPLICIT
-                // Use an implicit SSL connection (without the AUTHTLS command)
-                ftpServer.ConfigureConnection += (s, e) =>
+
+                if (options.ImplicitFtps)
                 {
-                    var sslStream = new SslStream(e.Connection.OriginalStream);
-                    sslStream.AuthenticateAsServer(cert);
-                    e.Connection.SocketStream = sslStream;
-                };
-#endif
+                    var authTlsOptions = serviceProvider.GetRequiredService<IOptions<AuthTlsOptions>>();
+                    // Use an implicit SSL connection (without the AUTHTLS command)
+                    ftpServer.ConfigureConnection += (s, e) =>
+                    {
+                        var sslStream = new SslStream(e.Connection.OriginalStream);
+                        sslStream.AuthenticateAsServer(authTlsOptions.Value.ServerCertificate);
+                        e.Connection.SocketStream = sslStream;
+                    };
+                }
+
                 try
                 {
                     // Start the FTP server
@@ -124,6 +171,37 @@ namespace TestFtpServer
                     Console.Error.WriteLine(ex);
                 }
             }
+        }
+
+        private static IServiceCollection CreateServices(TestFtpServerOptions options)
+        {
+            var services = new ServiceCollection();
+            services.AddLogging(cfg => cfg.SetMinimumLevel(LogLevel.Trace));
+            services.AddOptions();
+
+            services.Configure<AuthTlsOptions>(opt =>
+            {
+                if (options.ServerCertificateFile != null)
+                {
+                    opt.ServerCertificate = new X509Certificate2(
+                        options.ServerCertificateFile,
+                        options.ServerCertificatePassword);
+                }
+            });
+
+            services.Configure<FtpConnectionOptions>(opt => opt.DefaultEncoding = Encoding.ASCII);
+            services.Configure<FtpServerOptions>(opt =>
+            {
+                opt.ServerAddress = options.ServerAddress ?? "localhost";
+                opt.Port = options.GetPort();
+            });
+
+            return services;
+        }
+
+        private static IFtpServerBuilder Configure(IFtpServerBuilder builder)
+        {
+            return builder.EnableAnonymousAuthentication();
         }
     }
 }
