1ff0e79b3cea9422239b844101640e7a1042ae95
Mark Junker <m.junker@fubar-dev.de>
2019-01-28 09:45:08 +0100

    Added ADAT command handler

---
+++ src/FubarDev.FtpServer.Commands/CommandHandlers/AdatCommandHandler.cs
@@ -0,0 +1,55 @@
+// <copyright file="AdatCommandHandler.cs" company="Fubar Development Junker">
+// Copyright (c) Fubar Development Junker. All rights reserved.
+// </copyright>
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using JetBrains.Annotations;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FubarDev.FtpServer.CommandHandlers
+{
+    /// <summary>
+    /// The <c>ADAT</c> command handler.
+    /// </summary>
+    public class AdatCommandHandler : FtpCommandHandler
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdatCommandHandler"/> class.
+        /// </summary>
+        /// <param name="connectionAccessor">The FTP connection accessor.</param>
+        public AdatCommandHandler([NotNull] IFtpConnectionAccessor connectionAccessor)
+            : base(connectionAccessor, "ADAT")
+        {
+        }
+
+        /// <inheritdoc />
+        public override bool IsLoginRequired => false;
+
+        /// <inheritdoc />
+        public override Task<FtpResponse> Process(FtpCommand command, CancellationToken cancellationToken)
+        {
+            var loginStateMachine = Connection.ConnectionServices.GetRequiredService<IFtpLoginStateMachine>();
+            var authenticationMechanism = loginStateMachine.SelectedAuthenticationMechanism;
+            if (authenticationMechanism == null)
+            {
+                return Task.FromResult(new FtpResponse(503, T("Bad sequence of commands")));
+            }
+
+            byte[] data;
+            try
+            {
+                data = Convert.FromBase64String(command.Argument);
+            }
+            catch (FormatException)
+            {
+                return Task.FromResult(new FtpResponse(501, T("Syntax error in parameters or arguments.")));
+            }
+
+            return authenticationMechanism.HandleAdatAsync(data, cancellationToken);
+        }
+    }
+}
