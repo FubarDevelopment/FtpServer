d8783aeb71619e6778f2ef0a8c04dd56989a51b0
Mark Junker <m.junker@fubar-dev.de>
2019-05-01 18:50:16 +0200

    NuGet package updates

--- src/FubarDev.FtpServer.MembershipProvider.Pam/PamMembershipProviderOptions.cs
+++ src/FubarDev.FtpServer.MembershipProvider.Pam/PamMembershipProviderOptions.cs
@@ -1,6 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+// <copyright file="PamMembershipProviderOptions.cs" company="Fubar Development Junker">
+// Copyright (c) Fubar Development Junker. All rights reserved.
+// </copyright>
 
 namespace FubarDev.FtpServer.MembershipProvider.Pam
 {
