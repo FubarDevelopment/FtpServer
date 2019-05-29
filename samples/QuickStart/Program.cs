0dd1020b7b90154dffb5872c9101a45138f06068
Mark Junker <m.junker@fubar-dev.de>
2019-05-29 03:36:43 +0200

    Restored QuickStart example application

--- samples/QuickStart/Program.cs
+++ samples/QuickStart/Program.cs
@@ -8,9 +8,6 @@ using FubarDev.FtpServer;
 using FubarDev.FtpServer.FileSystem.DotNet;
 
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
-using Serilog;
 
 namespace QuickStart
 {
@@ -18,21 +15,8 @@ namespace QuickStart
     {
         static async Task Main()
         {
-            Log.Logger = new LoggerConfiguration()
-               .Enrich.FromLogContext()
-               .MinimumLevel.Verbose()
-               .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3} {SourceContext:l}] {Message:lj}{NewLine}{Exception}")
-               .CreateLogger();
-
             // Setup dependency injection
             var services = new ServiceCollection();
-
-            services
-               .AddLogging(lb =>
-                {
-                    lb.AddSerilog(dispose: true);
-                    lb.SetMinimumLevel(LogLevel.Trace);
-                });
 
             // use %TEMP%/TestFtpServer as root folder
             services.Configure<DotNetFileSystemOptions>(opt => opt
@@ -47,8 +31,6 @@ namespace QuickStart
 
             // Configure the FTP server
             services.Configure<FtpServerOptions>(opt => opt.ServerAddress = "*");
-
-            services.Configure<AuthTlsOptions>(opt => opt.ServerCertificate = new X509Certificate2("localhost.pfx", (string)null, X509KeyStorageFlags.Exportable));
 
             // Build the service provider
             using (var serviceProvider = services.BuildServiceProvider())
