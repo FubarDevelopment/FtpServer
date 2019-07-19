8a7ce457191aa99cece8dd3d12e1abf05d53f288
Mark Junker <m.junker@fubar-dev.de>
2019-07-19 16:23:09 +0200

    Renamed logger extensions to make it easier to search for

--- src/FubarDev.FtpServer/FtpConnection.cs
+++ src/FubarDev.FtpServer/FtpConnection.cs
@@ -563,7 +563,7 @@ namespace FubarDev.FtpServer
 
                         while (commandReader.TryRead(out var command))
                         {
-                            _logger?.Command(command);
+                            _logger?.LogCommand(command);
                             var context = new FtpContext(command, _serverCommandChannel, this);
                             await requestDelegate(context)
                                .ConfigureAwait(false);
