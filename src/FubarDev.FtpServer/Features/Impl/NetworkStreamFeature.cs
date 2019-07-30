407e4a1f544d058b408ac0b7bcc5a36ea8171993
Mark Junker <m.junker@fubar-dev.de>
2019-07-30 09:54:17 +0200

    Stream reader and writer can be internal to the connection.

--- src/FubarDev.FtpServer/Features/Impl/NetworkStreamFeature.cs
+++ src/FubarDev.FtpServer/Features/Impl/NetworkStreamFeature.cs
@@ -12,34 +12,14 @@ namespace FubarDev.FtpServer.Features.Impl
     {
         public NetworkStreamFeature(
             IFtpSecureConnectionAdapter secureConnectionAdapter,
-            IFtpService streamReaderService,
-            IFtpService streamWriterService,
             PipeWriter output)
         {
-            StreamReaderService = streamReaderService;
-            StreamWriterService = streamWriterService;
             Output = output;
             SecureConnectionAdapter = secureConnectionAdapter;
         }
 
         /// <inheritdoc />
         public IFtpSecureConnectionAdapter SecureConnectionAdapter { get; }
-
-        /// <summary>
-        /// Gets the stream reader service.
-        /// </summary>
-        /// <remarks>
-        /// It writes data from the network stream into a pipe.
-        /// </remarks>
-        public IFtpService StreamReaderService { get; }
-
-        /// <summary>
-        /// Gets the stream writer service.
-        /// </summary>
-        /// <remarks>
-        /// It reads data from the pipe and writes it to the network stream.
-        /// </remarks>
-        public IFtpService StreamWriterService { get; }
 
         /// <inheritdoc />
         public PipeWriter Output { get; }
