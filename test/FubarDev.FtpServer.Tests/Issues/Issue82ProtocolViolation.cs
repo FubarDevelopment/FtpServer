d7a077248449c055521d6f92fef1a27b2a649438
Mark Junker <m.junker@fubar-dev.de>
2019-11-14 18:02:23 +0100

    Trying to reproduce spurious protocol violations

--- test/FubarDev.FtpServer.Tests/Issues/Issue82ProtocolViolation.cs
+++ test/FubarDev.FtpServer.Tests/Issues/Issue82ProtocolViolation.cs
@@ -2,10 +2,13 @@
 // Copyright (c) Fubar Development Junker. All rights reserved.
 // </copyright>
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+
+using Polly;
 
 using Xunit;
 
@@ -23,23 +26,54 @@ namespace FubarDev.FtpServer.Tests.Issues
         [Fact]
         public async Task TestParallelRequests()
         {
+            var step = 50;
+            for (var i = 0; i != 5000; i += step)
+            {
+                await TestParallel(step, i).ConfigureAwait(false);
+            }
+        }
+
+        private async Task TestParallel(int maxTasks, int offset)
+        {
+            var rng = new Random();
+            var requestPolicy = Policy
+               .Handle<WebException>(e => e.Status != WebExceptionStatus.ProtocolError)
+               .WaitAndRetryAsync(5, count => TimeSpan.FromMilliseconds(rng.Next(1, 1001)));
+
             var tasks = new List<Task>();
-            var maxTasks = 100;
             for (var i = 0; i != maxTasks; i++)
             {
-                var task = Task.Run(GetFilesAsync);
+                var requestIndex = i;
+                var task = Task.Run(() => requestPolicy.ExecuteAsync(() => GetFilesAsync(offset + requestIndex)));
                 tasks.Add(task);
             }
 
-            await Task.WhenAll(tasks).ConfigureAwait(false);
+            var exceptions = new List<Exception>();
+            for (var requestIndex = 0; requestIndex != tasks.Count; ++requestIndex)
+            {
+                var task = tasks[requestIndex];
+                try
+                {
+                    await task.ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(new Exception($"Failed request {offset + requestIndex}: {ex.Message}", ex));
+                }
+            }
+
+            if (exceptions.Count != 0)
+            {
+                throw new AggregateException($"Failed tasks: {exceptions.Count}", exceptions);
+            }
         }
 
-        private async Task<IList<string>> GetFilesAsync()
+        private async Task<IList<string>> GetFilesAsync(int index)
         {
             var requestUri = $"ftp://127.0.0.1:{_server.Port}";
             var request = (FtpWebRequest)WebRequest.Create(requestUri);
             request.Method = WebRequestMethods.Ftp.ListDirectory;
-            request.Credentials = new NetworkCredential("anonymous", "foo@bar.com");
+            request.Credentials = new NetworkCredential("anonymous", $"foo_{index:D5}@bar.com");
             request.KeepAlive = false;
 
             var files = new List<string>();
